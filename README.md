f0d160e7c49b3c8db02e85fb26c2c19718813ff1
Mark Robinson <mark.robinson@mls.uzh.ch>
2025-08-26 16:07:53 +0200

    clear sketch of vizbench main wrapper

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# jessica-benchmark
+# vizbench-r
+
+This repo is where the code to call ALL R functions/packages is for the vizbench benchmark (designed by Jessica Jingyi Li's group and in particular, Zhiqian Zai and Qingyang Wang). For the omnibenchmark side, it's organized as a master repo that handles all (R) aspects, from deriving the datasets (usually, from public repos), running preprocessing methods (normalization, integration), and running visualization methods and then computing metrics.
+
+TODO: add some docs here to this page to highlight how all the arguments are handled/designed.
---
+++ run_vizbench.R
@@ -0,0 +1,75 @@
+#!/usr/bin/env Rscript
+
+## Originally sketched by Izaskun Mallona
+## modified/populated by Mark Robinson
+## various chunks of code from Zhiqian Zhai and Qingyang Wang
+## from original repo: https://github.com/zhiqianZ/Benchmark-Normalization-Integration-Visualization
+
+## Usage:
+## to do a system call to showcase how to call other subscripts
+##    Rscript thisfilename.R --what type --routine specific-module
+
+library(argparse)
+
+parser <- ArgumentParser(description = "Benchmarking entrypoint")
+
+# define arguments
+parser$add_argument("--what", 
+                    choices = c("rawdata", "simulation"),
+                    # choices = c("rawdata", "simulation", "normalization", "integration", "metric"), 
+                    required = TRUE, 
+                    help = "Module type: rawdata, simulation, normalizaiton, integration, viz, metric")
+
+#s <- switch(args$what, rawdata = c("mouse_pancreas"),
+#                       simulation = c("scdesign3"))
+#cat(s, "\n")
+# TODO: add subparser via 
+# subparsers = parser.add_subparsers (Python)
+# see https://stackoverflow.com/questions/9505898/conditional-command-line-arguments-in-python-using-argparse
+
+parser$add_argument("--routine", choices = c("mouse_pancreas", "scdesign3"),
+                    required = TRUE, help = "Module to run: name depends on the 'what'")
+
+parser$add_argument("--params", type = "character", default = "",
+                    help = "Optional parameters as free-form text")
+
+parser$add_argument("--verbose", type = "logical", default = TRUE,
+                    help = "Optional parameters as free-form text")
+
+args <- parser$parse_args()
+cat("Selected category: ", args$what, "\n")
+cat("Routine selected: ", args$routine, "\n")
+cat("Additional parameters: ", args$params, "\n")
+cat("Verbose: ", args$verbose, "\n")
+
+# source helper functions
+helpers <- file.path("utils", paste0(args$what, "_utils.R"))
+if( file.exists(helpers) ) {
+    message("Sourcing .. ", helpers)
+    source(helpers)
+} else {
+    message("Helper code not available. Exiting.")
+    quit("no", status = 1)
+}
+
+# check if implemented: throw error if not; run if so
+fun <- tryCatch(get(args$routine), error = function(e) e)
+if ( !("error" %in% class(fun)) ) {
+    x <- fun(args) # execute function
+    print(x)
+} else {
+    message('Unimplemented functionality. Exiting.\n') # throw error?
+    quit("no", status = 1)
+}
+
+if (args$what == 'rawdata') {
+    # 'x' should be a SingleCellExperiment object
+    # write it out in non-proprietary format
+    message("do something with sce object.\n")
+} else if (args$what == 'simulation') {
+    # 'x' is something here
+} else if (args$what == 'simulation') {
+    # 'x' is something here
+} else if (args$what == 'metric') {
+    # 'x' is something here
+}
