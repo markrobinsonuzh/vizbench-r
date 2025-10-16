e926c08907a9536941e6f225dfa94be517ea000d
Mark Robinson <mark.robinson@mls.uzh.ch>
2025-10-16 15:42:14 +0200

    not enough pcs

--- utils/visualize_utils.R
+++ utils/visualize_utils.R
@@ -3,7 +3,7 @@ load_pkgs <- function() {
   library(Seurat)
 }
 
-SeuratUMAP = function(args, n.pcs=50, n.cores = 10){
+SeuratUMAP = function(args, n.pcs=20, n.cores = 10){
   message("Running SeuratUMAP")
   seurat.obj <- read_seurat(args$integrate.ad)
   seurat.obj = RunUMAP(seurat.obj, dims = 1:n.pcs, 
