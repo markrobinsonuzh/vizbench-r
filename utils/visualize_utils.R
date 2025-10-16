32902c2cba0350e8705dec7ff0ddb2d6610f00e4
Mark Robinson <mark.robinson@mls.uzh.ch>
2025-10-16 15:35:18 +0200

    did not actually add utils

---
+++ utils/visualize_utils.R
@@ -0,0 +1,30 @@
+
+load_pkgs <- function() {
+  library(Seurat)
+}
+
+SeuratUMAP = function(args, n.pcs=50, n.cores = 10){
+  message("Running SeuratUMAP")
+  seurat.obj <- read_seurat(args$integrate.ad)
+  seurat.obj = RunUMAP(seurat.obj, dims = 1:n.pcs, 
+                       reduction="integrated",
+                       umap.method = "umap-learn",
+                       n_threads = n.cores)
+  Embeddings(seurat.obj,"umap")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
